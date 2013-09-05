7c93f78334671a18e25bb8ba1548fac4666617d3
muneerkk66 <muneer@ceino.com>
2013-09-05 16:30:38 +0530

    Initial Commit

---
+++ ShadowImageView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShadowImageView
+//
+//  Created by CEINO on 9/5/13.
+//  Copyright (c) 2013 CEINO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ShadowImageView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ShadowImageView
+//
+//  Created by CEINO on 9/5/13.
+//  Copyright (c) 2013 CEINO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
